3d74364ad094c3cb3c47b67b6ad228b6977f6d18
timocheu <ccs.timotheoche@gmail.com>
2026-04-12 21:36:15 +0800

    Add request-response protocol

--- client.cpp
+++ client.cpp
@@ -26,17 +26,58 @@ int main() {
         die("connect");
     }
 
-    char msg[] = "hello";
-    write(fd, msg, strlen(msg));
-
-    char rbuf[64] = {};
-    ssize_t n = read(fd, rbuf, sizeof(rbuf)-1);
-    if (n < 0) {
-        die("read");
+    int32_t err = query(fd, "Hello1");
+    if (err) {
+        goto L_DONE;
     }
 
-    printf("server says: %s\n", rbuf);
+    err = query(fd, "Hello2");
+    if (err) {
+        goto L_DONE;
+    }
+L_DONE:
     close(fd);
+    return 0;
+}
+
+const size_t k_max_msg = 4096;
+
+static int32_t query(int fd, const char *text) {
+    uint32_t len = (uint32_t)strlen(text);
+    if (len > k_max_msg) {
+        return -1; // Err message too long
+    }
+
+    // send request
+    char wbuf[4 + k_max_msg];
+    memcpy(wbuf, &len, 4);
+    memcpy(&wbuf[4], text, len);
+    if (int32_t err = write_all(fd, wbuf, 4 + len)) {
+        return err;
+    }
+
+    // 4 bytes
+    char rbuf[4 + k_max_msg + 1];
+    errno = 0;
+    int32_t err = read_full(fd, rbuf, 4);
+    if (err) {
+        msg(errno == 0 ? "EOF" : "read() error");
+        return err;
+    }
+    memcpy(&len, rbuf, 4);
+    if (len > k_max_msg) {
+        msg("too long");
+        return -1;
+    }
+
+    err = read_full(fd, &rbuf[4], len);
+    if (err) {
+        msg("read() error");
+        return error;
+    }
+
+    printf("server says: %.*s\n", len, &rbuf[4]);
+    return 0;
 }
 
 void die(const char *message) {
--- server.cpp
+++ server.cpp
@@ -36,7 +36,13 @@ int main() {
             continue;
         }
 
-        do_something(connfd);
+        while (true) {
+            int32_t err = one_request(connfd);
+            if (err) {
+                break;
+            }
+        }
+
         close(connfd);
     }
 
@@ -56,6 +62,72 @@ static void do_something(int connfd) {
 
     char wbuf[] = "world";
     write(connfd, wbuf, strlen(wbuf));
+}
+
+const size_t k_max_msg = 4096;
+
+void one_request(int connfd) {
+    char rbuf[4 + k_max_msg];
+    errno = 0;
+    int32_t err = read_full(connfd, rbuf, 4);
+    if (err) {
+        msg(errno == 0 ? "EOF" : "read() error");
+        return err;
+    }
+
+    uint_t len = 0;
+    memcpy(&len, rbuf, 4);
+
+    if (len > k_max_msg) {
+        msg("too long");
+        return -1;
+    }
+
+    err = read_full(connfd, &rbuf[4], len);
+    if (err) {
+        msg("read() error");
+        return err;
+    }
+
+    printf("client says: %.*\n", len, &rbuf[4]);
+    const char reply[] = "world";
+    char wbuf[4 + sizeof(reply)];
+    len = (uint32_t)strlen(reply);
+
+    // write the length in the first 4bytes;
+    memcpy(wbuf, &len, 4);
+    memcpy(&wbuf[4], &reply, len);
+
+    return write_all(connfd, wbuf, 4 + len);
+}
+
+void int32_t read_full(int fd, char *buf, size_t n) {
+    while (n > 0) {
+        ssize_t rv = read(fd, buf, n);
+        if (rv <= 0) {
+            return -1;
+        }
+
+        assert((size_t)rv <= n);
+
+        n -= (size_t)rv;
+
+        buf += rv;
+    }
+    return 0;
+}
+
+void int32_t write_all(int fd, const char *buf, size_t n) {
+    while (n > 0) {
+        ssize_t rv = write(fd, buf, n);
+        if (rv <= 0) {
+            return -1;
+        }
+        assert((size_t)rv <= n);
+        n -= (size_t)rv;
+        buf += rv;
+    }
+    return 0;
 }
 
 void die(const char *message) {
